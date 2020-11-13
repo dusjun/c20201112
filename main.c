dcea1fd5d1d82b7d0765635a8e10244c21d67439
dusjun <274640270@qq.com>
2020-11-13 15:56:34 +0800

    修改3

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-
-int main2()
-{
+int main(){
     printf("Hello world! win936 encode 第二 ( 2 ) 次修改\n");
+    printf("Hello world! win936 encode 第二 ( 3 ) 次修改\n");
     return 0;
 }
