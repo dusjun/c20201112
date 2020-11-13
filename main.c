b63a85050a212ccc0186709b4eee336bda52dcd8
dusjun <274640270@qq.com>
2020-11-13 15:05:58 +0800

    图形界面试试

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main2()
 {
-    printf("Hello world! 第一次修改 \n");
+    printf("Hello world! win936 encode 第二 ( 2 ) 次修改\n");
     return 0;
 }
